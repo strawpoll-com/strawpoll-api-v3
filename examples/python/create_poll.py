625e70142b05da72be1ef76df2faa948da7b85d9
gregorkrambs <33373220+gregorkrambs@users.noreply.github.com>
2022-05-06 13:42:22 +0200

    Create create_poll.py

---
+++ examples/python/create_poll.py
@@ -0,0 +1,57 @@
+import requests
+
+endpoint = "https://api.strawpoll.com/v3"
+api_key = "YOUR_API_KEY"
+
+payload = {
+	"title": "Is this a good Python example?",
+	"media": None,
+	"poll_options": [
+		{
+			"type": "text",
+			"value": "Yes"
+		},
+		{
+			"type": "text",
+			"value": "No"
+		},
+		{
+			"type": "text",
+			"value": "I don't know"
+		}
+	],
+	"poll_config": {
+		"vote_type": "default",
+		"allow_comments": True,
+		"allow_indeterminate": False,
+		"allow_other_option": False,
+		"custom_design_colors": None,
+		"deadline_at": None,
+		"duplication_checking": "ip",
+		"edit_vote_permissions": "nobody",
+		"force_appearance": None,
+		"hide_participants": False,
+		"is_multiple_choice": False,
+		"multiple_choice_min": None,
+		"multiple_choice_max": None,
+		"number_of_winners": 1,
+		"randomize_options": False,
+		"require_voter_names": False,
+		"results_visibility": "always",
+		"use_custom_design": False
+	},
+	"poll_meta": {
+		"description": "I don't write much Python myself.",
+		"location": None,
+	},
+	"type": "multiple_choice",
+}
+
+response = requests.post(endpoint + '/polls', json = payload, headers = { 'API-KEY': api_key })
+
+if response:
+	poll = response.json() # response is Poll object
+	print(poll["url"])
+else:
+	error = response.json()
+	print(error)
