66009fb070165202b4863cc45faab793e9ae9d6f
gregorkrambs <33373220+gregorkrambs@users.noreply.github.com>
2022-05-07 12:14:31 +0200

    Create create_meeting.js

---
+++ examples/javascript/create_meeting.js
@@ -0,0 +1,68 @@
+const axios = require('axios');
+
+const ENDPOINT = 'https://api.strawpoll.com/v3'
+const API_KEY = 'YOUR_API_KEY'
+
+let poll = {
+	"title": "When do we meet to discuss this JavaScript example?",
+	"media": null,
+	"poll_options": [
+		{
+			"type": "date",
+			"date": "2022-08-13"	// All-day option
+		},
+		{
+			"type": "time_range",
+			"start_time": 1660989600,
+			"end_time": 1660993200
+		},
+		{
+			"type": "time_range",
+			"start_time": new Date('2022-08-27T12:00:00+02:00').getTime() / 1000,	// new Date() reads UTC time so it is recommended to use a timezone offset (e.g. +02:00)
+			"end_time": null	// Open end
+		}
+	],
+	"poll_config": {
+		"vote_type": "participant_grid",
+		"allow_comments": true,
+		"allow_indeterminate": false,
+		"allow_other_option": false,
+		"custom_design_colors": null,
+		"deadline_at": null,
+		"duplication_checking": "ip",
+		"edit_vote_permissions": "nobody",
+		"force_appearance": null,
+		"hide_participants": false,
+		"is_multiple_choice": true,
+		"multiple_choice_min": 0,
+		"multiple_choice_max": 0,
+		"number_of_winners": 1,
+		"randomize_options": false,
+		"require_voter_names": true,
+		"results_visibility": "always",
+		"use_custom_design": false
+	},
+	"poll_meta": {
+		"description": null,
+		"location": "Online (Teams)",
+		"timezone": "Europe/Berlin"		// Use null for automatic timezones
+	},
+	"type": "meeting",
+}
+
+axios.post(ENDPOINT + '/polls', 
+	poll,
+	{
+		headers:
+			{
+				'Content-Type': 'application/json',
+				'X-API-KEY': API_KEY 
+			},
+	}
+)
+.then(res => {
+	console.log(res.data.url) // res.data is a Poll object
+})
+.catch(error => {
+	console.error(error)
+})
